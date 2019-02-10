2bbaa98ff3d1c54392e1ddbd44d20b9a79abf0da
technohard <you@example.com>
2019-02-11 00:54:38 +0700

    first commit

---
+++ platforms/browser/www/precache-manifest.5498aa25e563ce0ea606a9bdd14b68c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "8dee6bd8eade2b207a4b",
+    "url": "./static/js/main.8dee6bd8.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "./static/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "8dee6bd8eade2b207a4b",
+    "url": "./static/css/main.8dba1df1.chunk.css"
+  },
+  {
+    "revision": "aabc1b8dbcf922797ba3e7923a35b096",
+    "url": "./index.html"
+  }
+];
